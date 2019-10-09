ca2d030989f89933d768cc88b798c20252a90dc5
Mohamed Fathy <codetwostart@gmail.com>
2019-10-09 03:44:23 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace arithmeticoperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 2;
+            /*
+            Console.WriteLine("a + b = {0}", a + b);
+            Console.WriteLine("a - b = {0}", a - b);
+            Console.WriteLine("a * b = {0}", a * b);
+            Console.WriteLine("a / b = {0}", a / b);
+            Console.WriteLine("a % b = {0}", a % b);
+             */
+             //a = a + 2;
+             //a += 1;
+             //a++;
+             //Console.WriteLine(a++);//10
+             //Console.WriteLine(a);
+             int c = a++;
+             Console.WriteLine("c = "+c);
+             Console.WriteLine("a = " + a);
+             int d = --b;
+             Console.WriteLine("B = "+ b);
+             Console.WriteLine("d = "+ d);
+
+        }
+    }
+}
